1d8cd149e5caf0ea323b92a850207796f0576368
positronn <marcodelossantos@protonmail.com>
2023-05-07 12:08:27 -0600

    Notes on argument placeholding.

--- src/main/scala/progscala3/introscala/UpperMain2.scala
+++ src/main/scala/progscala3/introscala/UpperMain2.scala
@@ -2,6 +2,16 @@
 package progscala3.introscala
 
 @main def Hello2(params: String*): Unit =
-  /* `mkString`: concatenate the strings into a final strin. */
-  val output = params.map(_.toUpperCase).mkString(" ")
+  /* `mkString`: concatenate the strings into a final string. */
+  val output = params.map(_.toUpperCase).mkString("[", ", ", "]")
+  /* note the function passed to `map`. The following funciton literals
+  are esentually rhe same:
+    s => s.toUpperCase
+    _.toUpperCase
+  Rather than providing a name for the single argument, we can use _ as a placeholder.
+  This generalizes to functions with two or more arguments, where each
+  use of _ takes the place of one argument. 
+  This means that placeholders can’t be used if it’s necessary to refer
+  to any one of the arguments more than once.
+  */
   println(output)
